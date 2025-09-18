80a73b5f6fd9703f4f985b6ee5fd1b4e4f2930b0
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-09-17 22:52:39 -0400

    Base functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log('New tab page loaded!');
+    const searchForm = document.getElementById('search-form');
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the default form submission
+        searchBrave(); // Call the search function
+    });
+    const searchInput = document.getElementById('search-input');
+    searchInput.focus();
+    searchInput.select();
+
 });
+
+function searchBrave() {
+    var sb = document.getElementById("search-input");
+    var search = sb.value.replace(" ", "+");
+    window.location.replace("https://search.brave.com/search?q=" + search + "&source=web");
+}
+
+
+
 
